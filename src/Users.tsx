060e0466ac89eae3a322772a53c099cb3338d8af
Scott <mcleodrs@gmail.com>
2025-06-22 00:53:53 +0000

    Testing  users.tsx

---
+++ src/Users.tsx
@@ -0,0 +1,10 @@
+// src/UserProfile.tsx
+
+export default function UserProfile() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Thank you for your patience</h1>
+      <p>User Profile loading...</p>
+    </div>
+  );
+}
